b8d070388e0b9d57b25efef2826d04baf168642e
satej tupare <satejtupare@gmail.com>
2024-01-28 16:41:53 +0530

    deployed to azure storage

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,32 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   getVisitCount();
+  // const apiUrl =
+  //   "https://azureresumecountertej.azurewebsites.net/api/GetResumeCounter?code=Jx2Z3otnwb9yrxkfbjpk-4Iyn8ydSZaBoLnl8TyaYYVXAzFu117iWg==";
+
+  // fetch(apiUrl, { mode: "no-cors" })
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     return response.json(); // Parse the response body as JSON
+  //   })
+  //   .then((data) => {
+  //     console.log(data.tej); // Log the response data
+  //     // Process the response data here
+  //   })
+  //   .catch((error) => {
+  //     console.error("Fetch error:", error);
+  //   });
 });
 
+// const funntionUrl =
+//   "https://azureresumevisitorcounter.azurewebsites.net/api/GetVisitorCounter?";
+const functionUrl =
+  "https://azureresumecountertej.azurewebsites.net/api/GetResumeCounter?code=Jx2Z3otnwb9yrxkfbjpk-4Iyn8ydSZaBoLnl8TyaYYVXAzFu117iWg==";
 const localApiUrl = "http://localhost:7071/api/GetResumeCounter";
-
+// funntionUrl, { mode: "no-cors" }
 const getVisitCount = () => {
-  fetch(localApiUrl)
+  fetch(functionUrl)
     .then((response) => {
       if (!response.ok) {
         throw new Error("Failed to fetch visit count");
